a4b4ddf712c8c2383c49c330b85da5d1f330da13
Mateusz Wojtas <mateusz.wojtas@bpower2.com>
2023-10-03 12:52:52 +0000

    DataService caching metadata

--- src/main/kotlin/com/bprodactivv/dataservice/api/commands/SaveDataService.kt
+++ src/main/kotlin/com/bprodactivv/dataservice/api/commands/SaveDataService.kt
@@ -2,13 +2,13 @@ package com.bprodactivv.dataservice.api.commands
 
 import com.bprodactivv.dataservice.core.proql.ProQLCommandHandler
 import com.bprodactivv.dataservice.core.proql.models.ProQLCommand
+import org.springframework.cache.annotation.CachePut
 import org.springframework.stereotype.Service
 
 @Service
 class SaveDataService(
     private val proQLCommandHandler: ProQLCommandHandler
 ) {
-
     fun saveData(command: ProQLCommand): Any? {
         return proQLCommandHandler.save(command)
     }
